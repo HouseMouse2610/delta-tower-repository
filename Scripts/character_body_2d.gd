db0c86346aa0b9ea69ec67943cb47b4545e05958
House Mouse <isaacamfernandes4@gmail.com>
2025-12-23 09:36:08 -0300

    Arquivos importantes
    Importei todos os arquivos do delta tower para o git hub.

---
+++ Scripts/character_body_2d.gd
@@ -0,0 +1,55 @@
+extends CharacterBody2D
+
+
+@export var SPEED : int = 155
+@export var GRAVITY : int = 900
+@export var JUMP_FORCE : int = 255
+
+
+
+func _physics_process(_delta):
+	
+	
+	var direction = Input.get_axis("Left", "Right")
+	
+	
+	if direction:
+		
+		velocity.x = direction * SPEED
+		
+		
+		if is_on_floor():
+			$AnimatedSprite2D.play("Run")
+		
+		
+	else:
+		velocity.x = 0
+		
+		if is_on_floor():
+			$AnimatedSprite2D.play("Idle")
+	
+	
+	# Rotate
+	if direction == 1:
+		$AnimatedSprite2D.flip_h = false
+	elif direction == -1:
+		$AnimatedSprite2D.flip_h = true
+	
+	# Gravity
+	if not is_on_floor():
+		
+		velocity.y += GRAVITY * _delta
+		
+		if velocity.y > 0:
+			
+			$AnimatedSprite2D.play("Fall")
+	
+	
+	# Jump
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y -= JUMP_FORCE
+		
+		$AnimatedSprite2D.play("Jump")
+	
+	
+	move_and_slide()
